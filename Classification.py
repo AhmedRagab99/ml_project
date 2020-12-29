daf1c6602496003e1b69bccaf05bf3fef2fdd5a4
Ahmed ragab <aragab22@gmail.com>
2020-12-29 17:46:37 +0200

    feature:added classifier

---
+++ Classification.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, PolynomialFeatures, StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.utils import shuffle
+import seaborn as sns
+from sklearn.compose import ColumnTransformer
+from sklearn.svm import SVR, SVC
+from matplotlib import pyplot
+
+
+
+
+
+
+def Feature_Selection():
+    selector = SelectKBest(score_func=f_classif, k='all')
+    fitted = selector.fit(X, Y)
+    features_scores = pd.DataFrame(fitted.scores_)
+    features_columns = pd.DataFrame(X.columns)
+    best_features = pd.concat([features_columns, features_scores], axis=1)
+    best_features.columns = ['Feature', 'Score']
+    best_features.sort_values(by='Score', ascending=False, inplace=True)
+    new_best_features = []
+    feature = []
+    score = []
+    for i in range(5):
+        new_best_features.append(best_features['Feature'][i])
+        feature.append(best_features['Feature'][i])
+        score.append(best_features['Score'][i])
+    return new_best_features
+
+
+
+##read data
+df = pd.read_csv('spotify_training_classification.csv')
+
+# data preproccesing  exept the artist convertion with one_ hot_ encoder
+features = ['year', 'acousticness', 'energy', 'loudness', 'instrumentalness', 'duration_ms',
+            'explicit', 'tempo', 'valence', 'mode', 'liveness', 'speechiness']
+
+# replace nan values with the mean of the column
+df['year'] = df['year'].replace(np.nan, np.mean(df['year']))
+df['acousticness'] = df['acousticness'].replace(np.nan, np.mean(df['acousticness']))
+df['energy'] = df['energy'].replace(np.nan, np.mean(df['energy']))
+df['loudness'] = df['loudness'].replace(np.nan, np.mean(df['loudness']))
+df['instrumentalness'] = df['instrumentalness'].replace(np.nan, np.mean(df['instrumentalness']))
+df['duration_ms'] = df['duration_ms'].replace(np.nan, np.mean(df['duration_ms']))
+df['explicit'] = df['explicit'].replace(np.nan, np.mean(df['explicit']))
+df['mode'] = df['mode'].replace(np.nan, np.mean(df['mode']))
+df['tempo'] = df['tempo'].replace(np.nan, np.mean(df['tempo']))
+df['valence'] = df['valence'].replace(np.nan, np.mean(df['valence']))
+df['speechiness'] = df['speechiness'].replace(np.nan, np.mean(df['speechiness']))
+df['liveness'] = df['liveness'].replace(np.nan, np.mean(df['liveness']))
+
+training = df.sample(frac=0.8, random_state=420)
+##Assign features's value
+
+popularity = {'Intermediate': 0.5, 'High': 1,"Low":0}
+df.popularity_level = [popularity[item] for item in df.popularity_level]
+
+# replace strings with numbers
+
+
+
+X = training[features]
+Y = training['popularity_level']
+
+new_best_features = Feature_Selection()
+X_train, X_test, y_train, y_test = train_test_split(training[new_best_features], Y, test_size=0.2, random_state=420)
+
+error = []
+
+
+def KNNClasifier():
+    knn = KNeighborsClassifier(n_neighbors=100)
+    knn.fit(X_train, y_train)
+    print('KNN Accuracy:')
+    print(knn.score(X_test, y_test))
+    print('Error:')
+    for i in range(1, 40):
+        pred_i = knn.predict(X_test)
+        error.append(np.mean(pred_i != y_test))
+
+
+
+# Calculating error for K values between 1 and 40
+
+
+
+def DecistionTreeClassifier():
+    bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=2), algorithm="SAMME", n_estimators=300)
+    model = bdt.fit(X_train, y_train)
+    predict = bdt.predict(X_test)
+    print(' DecistionTree Accuracy:')
+    print(model.score(X_test, y_test))
+    print('Error:')
+    # print(mean_squared_error(y_test, predict))
+
+
+
+
+def PolyinomialKernalSVM():
+    svclassifier = SVC(kernel='poly',degree=8)
+    model1 = svclassifier.fit(X_train, y_train)
+    predict = model1.predict(X_test)
+    print('Accuracy for Polonomial :')
+    print(model1.score(X_test, y_test))
+    print('Error:')
+    print(mean_squared_error(y_test, predict))
+
+
+def GuasianKernalSVM():
+    svclassifier = SVC(kernel="rbf")
+    model1 = svclassifier.fit(X_train, y_train)
+    predict = model1.predict(X_test)
+    print('Accuracy for Guasioan :')
+    print(model1.score(X_test, y_test))
+    print('Error:')
+
+DecistionTreeClassifier()
+GuasianKernalSVM()
+PolyinomialKernalSVM()
+KNNClasifier()
--- SVM.py
+++ SVM.py
@@ -1,161 +1,123 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import numpy as np
-# from sklearn import metrics
-# from sklearn.linear_model import LogisticRegression, LinearRegression
-# from sklearn.metrics import accuracy_score, roc_auc_score
-# from sklearn.model_selection import train_test_split, cross_val_score
-# from sklearn.preprocessing import LabelEncoder, OneHotEncoder, PolynomialFeatures
-# from sklearn.utils import shuffle
-# from assignment_functions import *
-# import seaborn as sns
-#
-# df = pd.read_csv('spotify_training.csv')
-# pd.set_option("display.max.columns", None)
-# pd.set_option("display.precision", 2)
-# # print(df.keys())
-# # print(pd.isnull(df).sum())
-# #
-# # label_encoder = LabelEncoder()
-# # # Encode labels in column 'Country'.
-# # df['artists'] = pd.factorize(df['artists'], sort=True)[0]
-# #
-# # df['artists']= label_encoder.fit_transform(df['artists'])
-# # print(df['artists'])
-#
-# #
-# # label = LabelEncoder()
-# # df['popularity'] = pd.factorize(df['artists'], sort=True)[0]
-# #
-# #
-# # onehot_data = OneHotEncoder(sparse=False)
-# # onehot_data = onehot_data.fit_transform(df['artists'])
-# # print("Categorical data encoded into integer values....\n")
-# # print(onehot_data)
-#
-#
-#
-#
-#
-# LR = LinearRegression()
-# features = ['year','acousticness','artists','energy','loudness']
-# Y = df['popularity']
-#
-# # integer encode
-# label_encoder = LabelEncoder()
-# integer_encoded = label_encoder.fit_transform(Y)
-# # binary encode
-# onehot_encoder = OneHotEncoder(sparse=False)
-# integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-# df['artists'] = onehot_encoder.fit_transform(integer_encoded)
-# print(df['artists'])
-#
-#
-#
-# training = df.sample(frac = 0.8,random_state = 420)
-# X_train = training[features]
-# y_train = training['popularity']
-# X_test = df.drop(training.index)[features]
-# X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.2, random_state = 420)
-#
-# cls = LinearRegression()
-# # X=np.expand_dims(X_train, axis=1)
-# # Y=np.expand_dims(y_train, axis=1)
-# cls.fit(X_train,y_train) #Fit method is used for fitting your training data into the model
-# prediction= cls.predict(X_test)
-#
-# error = metrics.mean_squared_error(y_valid, prediction)
-# print(error)
-#
-#
-# # invert first example
-# # inverted = label_encoder.inverse_transform([np.argmax(onehot_encoded[0, :])])
-# # print(inverted)
-#
-# plt.figure(figsize=(10,6))
-# heatmap = sns.heatmap(df.corr(), vmin=-1,vmax=1, annot=True, cmap='viridis')
-# heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12)
-# plt.show()
-#
-# poly_reg = PolynomialFeatures(degree=3)
-# # X=np.expand_dims(X, axis=1)
-# # Y=np.expand_dims(Y, axis=1)
-# # X_poly = poly_reg.fit_transform(np.array(X))
-# # prediction= poly_reg.predict(X)
-# #
-# # error = metrics.mean_squared_error(Y, prediction)
-# # print(X_poly)
-#
-# # onehotencoder = OneHotEncoder()
-# # #reshape the 1-D country array to 2-D as fit_transform expects 2-D and finally fit the object
-# # X = onehotencoder.fit_transform(df.artists.values.reshape(2,120997)).toarray()
-# # #To add this back into the original dataframe
-# # dfOneHot = pd.DataFrame(X, columns = ["artists"+str(int(i)) for i in range(df.shape[1])])
-# # df = pd.concat([df, dfOneHot], axis=1)
-# # #droping the country column
-# # df= df.drop(['artist'], axis=1)
-# # #printing to verify
-# # print(df.head())
-# # # print(df.tail(),len(df))
-# # plt.figure(figsize=(10,6))
-# # heatmap = sns.heatmap(df.corr(), vmin=-1,vmax=1, annot=True, cmap='viridis')
-# # heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12)
-# # plt.show()
-# # target = df['variety']
-# # df = df.drop(df.index[rows])
-# #
-# # x = df['sepal.length']
-# # x2 = df['petal.length']
-# #
-# # setosa_x = x[:50]
-# # setosa_x2 = x2[:50]
-# #
-# # versicolor_x = x[50:]
-# # versicolor_x2 = x2[50:]
-# #
-# # plt.figure(figsize=(8,6))
-# # plt.scatter(setosa_x,setosa_x2,marker='+',color='green')
-# # plt.scatter(versicolor_x,versicolor_x2,marker='_',color='red')
-# # plt.show()
-# #
-# # ## Drop rest of the features and extract the target values
-# # df = df.drop(['sepal.width','petal.width'],axis=1)
-# # Y = []
-# # target = df['variety']
-# # for val in target:
-# #     if(val == 'Setosa'):
-# #         Y.append(-1)
-# #     else:
-# #         Y.append(1)
-# # df = df.drop(['variety'],axis=1)
-# # X = df.values.tolist()
-# # ## Shuffle and split the data into training and test set
-# # X, Y = shuffle(X,Y)
-# #
-# # X = np.array(X)
-# # Y = np.array(Y)
-# #
-# # Y = Y.reshape(100,1)
-# #
-# # y_pred,w = fit(X,Y)
-# #
-# # ## Predict
-# #
-# # predictions = []
-# # for val in y_pred:
-# #     if(val >= 0):
-# #         predictions.append(1)
-# #     else:
-# #         predictions.append(-1)
-# #
-# # print(accuracy_score(Y,predictions))
-# #
-# # #plot decision boundary
-# # min_f1 = min(df['sepal.length'])
-# # max_f1 = max(df['sepal.length'])
-# # x_values = [(min_f1 - 1),(max_f1 + 1)]
-# # y_values = - (w[0] + np.multiply(w[1], x_values)) / w[2]
-# # plt.plot(x_values, y_values, label='Decision Boundary')
-# # plt.scatter(setosa_x,setosa_x2,marker='+',color='green')
-# # plt.scatter(versicolor_x,versicolor_x2,marker='_',color='red')
-# # plt.show()
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, PolynomialFeatures, StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.utils import shuffle
+import seaborn as sns
+from sklearn.compose import ColumnTransformer
+from sklearn.svm import SVR
+from matplotlib import pyplot
+
+
+def Feature_Selection():
+    selector = SelectKBest(score_func=f_classif, k='all')
+    fitted = selector.fit(X, Y)
+    features_scores = pd.DataFrame(fitted.scores_)
+    features_columns = pd.DataFrame(X.columns)
+    best_features = pd.concat([features_columns, features_scores], axis=1)
+    best_features.columns = ['Feature', 'Score']
+    best_features.sort_values(by='Score', ascending=False, inplace=True)
+    new_best_features = []
+    feature = []
+    score = []
+    for i in range(5):
+        new_best_features.append(best_features['Feature'][i])
+        feature.append(best_features['Feature'][i])
+        score.append(best_features['Score'][i])
+
+    plt.figure(figsize=(10, 8))
+    sns.barplot(feature, score)
+    plt.ylabel('feature')
+    plt.xlabel('score')
+    plt.show()
+    return new_best_features
+
+
+##read data
+df = pd.read_csv('spotify_training.csv')
+
+##drop nulls
+df.dropna(inplace=True)
+
+## correlation heatmap
+correlations = df.corr()
+plt.figure(figsize=(10, 6))
+heatmap = sns.heatmap(correlations, vmin=-1, vmax=1, annot=True, cmap='viridis')
+heatmap.set_title('Correlation Heatmap', fontdict={'fontsize': 12}, pad=12)
+plt.show()
+
+##label and one-hot-encoder
+le = LabelEncoder()
+onehot_encoder = OneHotEncoder(sparse=False)
+for col in df.columns.values:
+    if df[col].dtypes == 'object':
+        df[col] = le.fit_transform(df[col].astype(str))
+
+integer_encoded_reshape = np.array(df['key']).reshape(len(df['key']), 1)
+onehot_encoder = OneHotEncoder(sparse=False)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded_reshape)
+df['key'] = onehot_encoded
+
+features = ['year', 'acousticness', 'energy', 'loudness', 'instrumentalness', 'duration_ms',
+            'explicit', 'tempo', 'valence', 'mode', 'liveness', 'speechiness']
+
+##Assign features's value
+training = df.sample(frac=0.6, random_state=420)
+X = training[features]
+Y = training['popularity']
+
+new_best_features = Feature_Selection()
+
+X_train, X_test, y_train, y_test = train_test_split(training[new_best_features], Y, test_size=0.4, random_state=420)
+
+
+def Linear_Regression():
+    regressor = LinearRegression()
+    model = regressor.fit(X_train, y_train)
+    y_pred = regressor.predict(X_test)
+    print('Accuracy:')
+    print(model.score(X_test, y_test))
+    print('Error:')
+    print(mean_squared_error(y_test, y_pred))
+
+    fig, axes = plt.subplots(1, len(X_train.columns), sharey=True, constrained_layout=True, figsize=(30, 15))
+
+    mn = min(len(X_train), len(y_pred))
+
+    for i, e in enumerate(X_train.columns):
+        newX_Train = X_train[e][: mn]
+        regressor.fit(newX_Train[:, np.newaxis], y_pred)
+        axes[i].set_title("linear regression")
+        axes[i].set_xlabel(str(e))
+        axes[i].set_ylabel('Predection')
+        axes[i].scatter(newX_Train[:, np.newaxis], y_pred, color='b')
+        axes[i].plot(newX_Train[:, np.newaxis],
+                     regressor.predict(newX_Train[:, np.newaxis]), color='k')
+
+    plt.show()
+
+
+Linear_Regression()
+
+
+def Polynomial_Regression():
+    poly_reg = PolynomialFeatures(degree=4)
+    X_poly = poly_reg.fit_transform(X_train)
+    pol_reg = LinearRegression()
+    pol_reg.fit(X_poly, y_train)
+    y_pred = pol_reg.predict(X_poly)
+    mse = np.sqrt(mean_squared_error(y_train, y_pred))
+    r2 = r2_score(y_train, y_pred)
+    print('Error:')
+    print(mse)
+    print('Accuracy:')
+    print(r2)
+
+
+Polynomial_Regression()
